0883978aec714267a3b69dd6bfd0d3a9a55ed33f
ru0ya <viktaangi02@gmail.com>
2023-05-15 13:52:29 +0300

    debug

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer
+arguments page and page_size.
+
+The function should return a tuple of size two containing
+a start index and an end index corresponding to the range of indexes to 
+return in a list for those particular pagination parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
+
+
+"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    Function that takes two arguments
+
+    Parameters: page(int), page_size(int)
+
+    Returns: Tuple of size two containing a start and end
+    index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    
+    return start_index, end_index
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+function named index_range that takes two integer
+arguments page and page_size.
+
+The function should return a tuple of size two containing
+a start index and an end index corresponding to the range of indexes to 
+return in a list for those particular pagination parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
+
+
+"""
+
+
+import csv
+import math
+import pandas as pd
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    Function that takes two arguments
+
+    Parameters: page(int), page_size(int)
+
+    Returns: Tuple of size two containing a start and end
+    index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        data = pd.read_csv("Popular_Baby_Names.csv")
+        assert page > 0
+        assert page_size > 0
+
+            
